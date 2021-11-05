5fa2673bc1782e2c2204bdcb7c948dd4cbbef306
youngmillan <aa.millan.r@edu.utc.mx>
2021-11-05 09:13:23 -0600

    lasumadedosnumeros

--- 01_Hola_Mundo/HolaMundo.java
+++ 01_Hola_Mundo/HolaMundo.java
@@ -1,31 +1,29 @@
-//esto es un comentario 
+//esto es un comentario
 
 /*
 esto es otro comentario los comentarios no van a afectar
-alcodigo fuente de la clase
+al codigo fuente de la clase
 */
 
 class HolaMundo{
-//cuerpo del programa
-/*
-aqui es donde nosotros empezamos a codifucar todas las funciones que 
-querramos para el programa
-su estructura es
-
-variables[globales, independientes o solo de la clase]
-
-metodos [acciones a programar]
-*/
-
-//toda clase debe de tener si solo si es 
-//la clase principal un metodo principal
-
-//elmetodo principal es donde se hace la llamada
-//a las acciones de todo el programa
-
-        public static void main(String[] args){
-        //esto es el cuerpo del metodo principal
-        System.out.print("Hola Mundo... :3");
-        }
-
-}
+        //cuerpo del programa
+        /*
+        aqui es donde nosotros empezamos a codificar todas las funciones
+        que querramos para el programa
+        su estructura es:
+        variables [globales, independientes o solo de la clase]
+        metodos [acciones a programar]
+        */
+        
+        //toda clase debe de tener si solo si es 
+        //la clase principal un metodo principal
+        
+        //el metodo principal es donde se hace la llamada
+        //a las acciones de todo el programa
+        
+                public static void main(String[] args){
+                //esto es el cuerpo del metodo principal
+                System.out.print("Hola Mundo... :3");
+                }
+        
+        } 
--- 03_suma2numeros/sumadedosnumeros.java
+++ 03_suma2numeros/sumadedosnumeros.java
@@ -1,16 +1,10 @@
 /*
-
 Para poder hacer el programa de la suma de dos numeros
-
 debemos de entender el algoritmo de la suma
-
 declarar variables
-
 num1 num2 resultado
-
 1.- solo una vez se sume?
 2.- voy a querer que se sumen los numeros y que se repita
-
 */ 
 
 //es necesario ocupar librerias dentro de java para algunos procesos
@@ -28,9 +22,7 @@ class sumadedosnumeros{
         Scanner entrada = new Scanner(System.in);
         /*
         Para realizar una instancia del objeto se debe de colocar:
-
         Nombreclase nombreobjeto = new Nombreclase();
-
         System.in Es una clase superior donde nosotros podemos obtener los 
         datos que vienen por parte de la entrada estandar de la computadora
         (por parte de la consola)
@@ -42,7 +34,7 @@ class sumadedosnumeros{
 
 
        do{
-        
+
         System.out.println("Introduce los numeros que quieras sumar; si pones 0 se detiene la suma: ");
         //asignar el primer numero a su variable
         num1 = entrada.nextInt();
@@ -52,11 +44,11 @@ class sumadedosnumeros{
 
         resultado = resultado + num1;  
 
-        
+
 
 
        }while(num1 !=0);
-        
+
        System.out.println("El resultado de la suma es: " + resultado);
     }
-}
+} 
